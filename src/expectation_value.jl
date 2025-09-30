b6e3f8cb061e76697e8f68f4a6fdd0cd5ffc437b
Nick Mayhall <nmayhall@iu.edu>
2025-09-29 23:22:04 -0400

    removed ^ from expectation value for efficiency

--- src/expectation_value.jl
+++ src/expectation_value.jl
@@ -1,6 +1,11 @@
 
 function expectation_value(p::Union{PauliBasis{N}, Pauli{N}}, ket::Ket{N}) where N
-    return (-1)^count_ones(p.z & ket.v) * (p.x == 0) * coeff(p)
+    if count_ones(p.z & ket.v)%2 == 0
+        return (p.x == 0) * coeff(p)
+    else
+        return -1*(p.x == 0) * coeff(p)
+    end
+    # return (-1)^count_ones(p.z & ket.v) * (p.x == 0) * coeff(p)
 end
 
 
@@ -8,7 +13,21 @@ end
 function expectation_value(p::Union{PauliBasis{N}, Pauli{N}}, d::Union{Dyad{N}, DyadBasis{N}}) where N
     sgn = count_ones(p.z & d.bra.v)  # sgn <j| = <j| z 
     val = d.ket.v ⊻ d.bra.v == p.x # <j|x|i>
-    return (-1)^sgn * val * coeff(p) * coeff(d) * 1im^symplectic_phase(p)
+    sgn1 = 1
+    phs1 = 1
+    if sgn % 2 != 0
+        sgn1 = -1
+    end
+    if symplectic_phase(p) == 1
+        phs1 = 1im
+    elseif symplectic_phase(p) == 2
+        phs1 = -1
+    elseif symplectic_phase(p) == 3
+        phs1 = -1im
+    end
+
+    return sgn1 * phs1 * val * coeff(p) * coeff(d)
+    # return (-1)^sgn * val * coeff(p) * coeff(d) * 1im^symplectic_phase(p)
 end
 
 function expectation_value(p::PauliSum{N,T}, d::Union{Ket{N}, Dyad{N}, DyadBasis{N}}) where {N,T}
